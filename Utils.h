891d7d47b4bf84067df9cfd9d98297d5472e01a1
no bones about it <chronogene@gmail.com>
2015-02-02 15:32:16 -0800

    rewriting code from aie tuts

---
+++ Utils.h
@@ -0,0 +1,126 @@
+GLuint CreateShader(GLenum a_eShaderType, const char *a_strShaderFile)
+{
+	std::string strShaderCode;
+	//open shader file
+	std::ifstream shaderStream(a_strShaderFile);
+	//if that worked ok, load file line by line
+	if (shaderStream.is_open())
+	{
+		std::string Line = "";
+		while (std::getline(shaderStream, Line))
+		{
+			strShaderCode += "\n" + Line;
+		}
+		shaderStream.close();
+	}
+
+
+	//convert to cstring
+	char const *szShaderSourcePointer = strShaderCode.c_str();
+
+	//create shader ID
+	GLuint uiShader = glCreateShader(a_eShaderType);
+	//load source code
+	glShaderSource(uiShader, 1, &szShaderSourcePointer, NULL);
+
+	//compile shader
+	glCompileShader(uiShader);
+
+	//check for compilation errors and output them
+	GLint iStatus;
+	glGetShaderiv(uiShader, GL_COMPILE_STATUS, &iStatus);
+	if (iStatus == GL_FALSE)
+	{
+		GLint infoLogLength;
+		glGetShaderiv(uiShader, GL_INFO_LOG_LENGTH, &infoLogLength);
+
+		GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+		glGetShaderInfoLog(uiShader, infoLogLength, NULL, strInfoLog);
+
+		const char *strShaderType = NULL;
+		switch (a_eShaderType)
+		{
+		case GL_VERTEX_SHADER: strShaderType = "vertex"; break;
+		case GL_FRAGMENT_SHADER: strShaderType = "fragment"; break;
+		}
+
+		fprintf(stderr, "Compile failure in %s shader:\n%s\n", strShaderType, strInfoLog);
+		delete[] strInfoLog;
+	}
+
+	return uiShader;
+}
+
+GLuint CreateProgram(const char *a_vertex, const char *a_frag)
+{
+	std::vector<GLuint> shaderList;
+
+	shaderList.push_back(CreateShader(GL_VERTEX_SHADER, a_vertex));
+	shaderList.push_back(CreateShader(GL_FRAGMENT_SHADER, a_frag));
+
+	//create shader program ID
+	GLuint uiProgram = glCreateProgram();
+
+	//attach shaders
+	for (auto shader = shaderList.begin(); shader != shaderList.end(); shader++)
+		glAttachShader(uiProgram, *shader);
+
+	//link program
+	glLinkProgram(uiProgram);
+
+	//check for link errors and output them
+	GLint status;
+	glGetProgramiv(uiProgram, GL_LINK_STATUS, &status);
+	if (status == GL_FALSE)
+	{
+		GLint infoLogLength;
+		glGetProgramiv(uiProgram, GL_INFO_LOG_LENGTH, &infoLogLength);
+
+		GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+		glGetProgramInfoLog(uiProgram, infoLogLength, NULL, strInfoLog);
+		fprintf(stderr, "Linker failure: %s\n", strInfoLog);
+		delete[] strInfoLog;
+	}
+
+	for (auto shader = shaderList.begin(); shader != shaderList.end(); shader++)
+	{
+		glDetachShader(uiProgram, *shader);
+		glDeleteShader(*shader);
+	}
+
+	return uiProgram;
+}
+
+unsigned int loadTexture(const char* a_pFilename, int & a_iWidth, int & a_iHeight, int & a_iBPP)
+{
+	unsigned int uiTextureID = 0;
+	//check file exists
+	if (a_pFilename != nullptr)
+	{
+		//read in image data from file
+		unsigned char* pImageData = SOIL_load_image(a_pFilename, &a_iWidth, &a_iHeight, &a_iBPP, SOIL_LOAD_AUTO);
+
+		//check for successful read
+		if (pImageData)
+		{
+			//create opengl texture handle
+			uiTextureID = SOIL_create_OGL_texture(pImageData, a_iWidth, a_iHeight, a_iBPP,
+				SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT);
+			//clear what was read in from file now that it is stored in the handle
+			SOIL_free_image_data(pImageData);
+		}
+
+		//check for errors
+		if (uiTextureID == 0)
+		{
+			std::cerr << "SOIL loading error: " << SOIL_last_result() << std::endl;
+		}
+		return uiTextureID;
+	}
+}
+
+
+
+static int windowWidth = 1024, windowHeight = 720;
+void Update();
+KeyStater Keys;
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <GL\glew.h>
-#include <GL\wglew.h>
+//#include <GL\wglew.h>
 
 #include "KeyStater.h"
 
@@ -14,162 +14,7 @@
 #include <vector>
 #include <string>
 #include <fstream>
-
-GLuint CreateShader(GLenum a_eShaderType, const char *a_strShaderFile)
-{
-	std::string strShaderCode;
-	//open shader file
-	std::ifstream shaderStream(a_strShaderFile);
-	//if that worked ok, load file line by line
-	if (shaderStream.is_open())
-	{
-		std::string Line = "";
-		while (std::getline(shaderStream, Line))
-		{
-			strShaderCode += "\n" + Line;
-		}
-		shaderStream.close();
-	}
-
-
-	//convert to cstring
-	char const *szShaderSourcePointer = strShaderCode.c_str();
-
-	//create shader ID
-	GLuint uiShader = glCreateShader(a_eShaderType);
-	//load source code
-	glShaderSource(uiShader, 1, &szShaderSourcePointer, NULL);
-
-	//compile shader
-	glCompileShader(uiShader);
-
-	//check for compilation errors and output them
-	GLint iStatus;
-	glGetShaderiv(uiShader, GL_COMPILE_STATUS, &iStatus);
-	if (iStatus == GL_FALSE)
-	{
-		GLint infoLogLength;
-		glGetShaderiv(uiShader, GL_INFO_LOG_LENGTH, &infoLogLength);
-
-		GLchar *strInfoLog = new GLchar[infoLogLength + 1];
-		glGetShaderInfoLog(uiShader, infoLogLength, NULL, strInfoLog);
-
-		const char *strShaderType = NULL;
-		switch (a_eShaderType)
-		{
-		case GL_VERTEX_SHADER: strShaderType = "vertex"; break;
-		case GL_FRAGMENT_SHADER: strShaderType = "fragment"; break;
-		}
-
-		fprintf(stderr, "Compile failure in %s shader:\n%s\n", strShaderType, strInfoLog);
-		delete[] strInfoLog;
-	}
-
-	return uiShader;
-}
-
-GLuint CreateProgram(const char *a_vertex, const char *a_frag)
-{
-	std::vector<GLuint> shaderList;
-
-	shaderList.push_back(CreateShader(GL_VERTEX_SHADER, a_vertex));
-	shaderList.push_back(CreateShader(GL_FRAGMENT_SHADER, a_frag));
-
-	//create shader program ID
-	GLuint uiProgram = glCreateProgram();
-
-	//attach shaders
-	for (auto shader = shaderList.begin(); shader != shaderList.end(); shader++)
-		glAttachShader(uiProgram, *shader);
-
-	//link program
-	glLinkProgram(uiProgram);
-
-	//check for link errors and output them
-	GLint status;
-	glGetProgramiv(uiProgram, GL_LINK_STATUS, &status);
-	if (status == GL_FALSE)
-	{
-		GLint infoLogLength;
-		glGetProgramiv(uiProgram, GL_INFO_LOG_LENGTH, &infoLogLength);
-
-		GLchar *strInfoLog = new GLchar[infoLogLength + 1];
-		glGetProgramInfoLog(uiProgram, infoLogLength, NULL, strInfoLog);
-		fprintf(stderr, "Linker failure: %s\n", strInfoLog);
-		delete[] strInfoLog;
-	}
-
-	for (auto shader = shaderList.begin(); shader != shaderList.end(); shader++)
-	{
-		glDetachShader(uiProgram, *shader);
-		glDeleteShader(*shader);
-	}
-
-	return uiProgram;
-}
-
-unsigned int loadTexture(const char* a_pFilename, int & a_iWidth, int & a_iHeight, int & a_iBPP)
-{
-	unsigned int uiTextureID = 0;
-	//check file exists
-	if (a_pFilename != nullptr)
-	{
-		//read in image data from file
-		unsigned char* pImageData = SOIL_load_image(a_pFilename, &a_iWidth, &a_iHeight, &a_iBPP, SOIL_LOAD_AUTO);
-
-		//check for successful read
-		if (pImageData)
-		{
-			//create opengl texture handle
-			uiTextureID = SOIL_create_OGL_texture(pImageData, a_iWidth, a_iHeight, a_iBPP,
-				SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT);
-			//clear what was read in from file now that it is stored in the handle
-			SOIL_free_image_data(pImageData);
-		}
-
-		//check for errors
-		if (uiTextureID == 0)
-		{
-			std::cerr << "SOIL loading error: " << SOIL_last_result() << std::endl;
-		}
-		return uiTextureID;
-	}
-}
-
-
-float* getOrtho(float left, float right, float bottom, float top, float a_fNear, float a_fFar)
-{
-	//to correspond with mat4 in the shader
-	//ideally this function would be part of your matrix class
-	//however I wasn't willing to write your matrix class for you just to show you this
-	//so here we are in array format!
-	//add this to your matrix class as a challenge if you like!
-	float* toReturn = new float[12];
-	toReturn[0] = 2.0 / (right - left);;
-	toReturn[1] = toReturn[2] = toReturn[3] = toReturn[4] = 0;
-	toReturn[5] = 2.0 / (top - bottom);
-	toReturn[6] = toReturn[7] = toReturn[8] = toReturn[9] = 0;
-	toReturn[10] = 2.0 / (a_fFar - a_fNear);
-	toReturn[11] = 0;
-	toReturn[12] = -1 * ((right + left) / (right - left));
-	toReturn[13] = -1 * ((top + bottom) / (top - bottom));
-	toReturn[14] = -1 * ((a_fFar + a_fNear) / (a_fFar - a_fNear));
-	toReturn[15] = 1;
-	return toReturn;
-}
-
-void Update();
-KeyStater Keys;
-
-//declaring vertex struct
-struct Vertex
-{
-	float fPositions[4];
-	float fColours[4];
-	float fUVs[2];
-};
-
-
+#include "Utils.h"
 
 int main()
 {
@@ -182,7 +27,7 @@ int main()
 
 	//create a windowed mode window and it's OpenGL context
 	GLFWwindow* window;
-	window = glfwCreateWindow(1024, 720, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(windowWidth, windowHeight, "Hello World", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -199,188 +44,18 @@ int main()
 		return -1;
 	}
 
-	//creating vertices
-	Vertex* myShape = new Vertex[3];
-	myShape[0].fPositions[0] = 520.0f;
-	myShape[0].fPositions[1] = 380.0f;
-	myShape[1].fPositions[0] = 500.0f;
-	myShape[1].fPositions[1] = 340.0f;
-	myShape[2].fPositions[0] = 540.0f;
-	myShape[2].fPositions[1] = 340.0f;
-	for (int i = 0; i < 3; i++)
-	{
-		myShape[i].fPositions[2] = 0.0f;
-		myShape[i].fPositions[3] = 1.0f;
-		myShape[i].fColours[0] = 1.0f;
-		myShape[i].fColours[1] = 1.0f;
-		myShape[i].fColours[2] = 1.0f;
-		myShape[i].fColours[3] = 1.0f;
-	}
-	//set up the UVs
-	myShape[0].fUVs[0] = 0.5f; //top of the triangle
-	myShape[0].fUVs[1] = 1.0f;
-	myShape[1].fUVs[0] = 0.0f; //bottom left
-	myShape[1].fUVs[1] = 0.0f;
-	myShape[2].fUVs[0] = 1.0f; //bottom right
-	myShape[2].fUVs[1] = 0.0f;
-
-	//create ID for a vertex buffer object
-	GLuint uiVBO;
-	glGenBuffers(1, &uiVBO);
-
-	if (uiVBO != 0)
-	{
-		//bind VBO
-		glBindBuffer(GL_ARRAY_BUFFER, uiVBO);
-		//allocate space for vertices on the graphics card
-		glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex)* 3, NULL, GL_STATIC_DRAW);
-		//get pointer to allocated space on the graphics card
-		GLvoid* vBuffer = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
-		//copy data to graphics card
-		memcpy(vBuffer, myShape, sizeof(Vertex)* 3);
-		//unmap and unbind buffer
-		glUnmapBuffer(GL_ARRAY_BUFFER);
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-	}
-
-	GLuint uiIBO;
-	glGenBuffers(1, &uiIBO);
-
-	//check it succeeded
-	if (uiIBO != 0)
-	{
-		//bind IBO
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, uiIBO);
-		//allocate space for index info on the graphics card
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * sizeof(char), NULL, GL_STATIC_DRAW);
-		//get pointer to newly allocated space on the graphics card
-		GLvoid* iBuffer = glMapBuffer(GL_ELEMENT_ARRAY_BUFFER, GL_WRITE_ONLY);
-		//specify the order we'd like to draw our vertices.
-		//In this case they are in sequential order
-		for (int i = 0; i < 3; i++)
-		{
-			((char*)iBuffer)[i] = i;
-		}
-		//unmap and unbind
-		glUnmapBuffer(GL_ELEMENT_ARRAY_BUFFER);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-	}
-
-	int width = 64, height = 64, bpp = 4;
-	GLuint uiTextureId = loadTexture("ian.png", width, height, bpp);
-
-	//create shader program
-	GLuint uiProgramTextured = CreateProgram("VertexShader.glsl", "TexturedFragmentShader.glsl");
-
-	//find the position of the matrix variable in the shader so we can send info there later
-	GLuint MatrixIDFlat = glGetUniformLocation(uiProgramTextured, "MVP");
-
-	//set up the mapping of the screen to pixel co-ordinates. Try changing these values to see what happens.
-	float *orthographicProjection = getOrtho(0, 1024, 0, 720, 0, 100);
 
 	//loop until the user closes the window
 	while (!glfwWindowShouldClose(window))
 	{
 		Update();
 
-		if (Keys.IsDown(VK_UP))
-		{
-			for (int i = 0; i < 3; i++)
-			{
-				myShape[i].fPositions[1] += .1f;
-			}
-			glBindBuffer(GL_ARRAY_BUFFER, uiVBO);
-			GLvoid* vBuffer = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
-			//copy data to graphics card
-			memcpy(vBuffer, myShape, sizeof(Vertex)* 3);
-			//unmap and unbind buffer
-			glUnmapBuffer(GL_ARRAY_BUFFER);
-			glBindBuffer(GL_ARRAY_BUFFER, 0);
-		}
-		if (Keys.IsDown(VK_DOWN))
-		{
-			for (int i = 0; i < 3; i++)
-			{
-				myShape[i].fPositions[1] -= .1f;
-			}
-			glBindBuffer(GL_ARRAY_BUFFER, uiVBO);
-			GLvoid* vBuffer = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
-			//copy data to graphics card
-			memcpy(vBuffer, myShape, sizeof(Vertex)* 3);
-			//unmap and unbind buffer
-			glUnmapBuffer(GL_ARRAY_BUFFER);
-			glBindBuffer(GL_ARRAY_BUFFER, 0);
-		}
-		if (Keys.IsDown(VK_LEFT))
-		{
-			for (int i = 0; i < 3; i++)
-			{
-				myShape[i].fPositions[0] -= .1f;
-			}
-			glBindBuffer(GL_ARRAY_BUFFER, uiVBO);
-			GLvoid* vBuffer = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
-			//copy data to graphics card
-			memcpy(vBuffer, myShape, sizeof(Vertex)* 3);
-			//unmap and unbind buffer
-			glUnmapBuffer(GL_ARRAY_BUFFER);
-			glBindBuffer(GL_ARRAY_BUFFER, 0);
-		}
-		if (Keys.IsDown(VK_RIGHT))
-		{
-			for (int i = 0; i < 3; i++)
-			{
-				myShape[i].fPositions[0] += .1f;
-			}
-			glBindBuffer(GL_ARRAY_BUFFER, uiVBO);
-			GLvoid* vBuffer = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
-			//copy data to graphics card
-			memcpy(vBuffer, myShape, sizeof(Vertex)* 3);
-			//unmap and unbind buffer
-			glUnmapBuffer(GL_ARRAY_BUFFER);
-			glBindBuffer(GL_ARRAY_BUFFER, 0);
-		}
-
-
 		//draw code goes here
 		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//bind Texture
-		glBindTexture(GL_TEXTURE_2D, uiTextureId);
-		glBindBuffer(GL_ARRAY_BUFFER, uiVBO);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, uiIBO);
-		glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
-		glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(float)* 4));
-		//now we have UVs to worry about, we need to send that info to the graphics card too
-		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(float)* 8));
 
-		//enable shaders
-		glUseProgram(uiProgramTextured);
 
-		glBindBuffer(GL_ARRAY_BUFFER, uiVBO);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, uiIBO);
-
-		glUniformMatrix4fv(MatrixIDFlat, 1, GL_FALSE, orthographicProjection);
-
-		//enable the vertex array states
-		glEnableVertexAttribArray(0);
-		glEnableVertexAttribArray(1);
-		glEnableVertexAttribArray(2);
-
-		/*Since the data is in the same array, we need to specify the gap between
-		vertices (A whole Vertex structure instance) and the offset of the data
-		from the beginning of the structure instance. The positions are at the
-		start, so their offset is 0. But the colours are after the positions, so
-		they are offset by the size of the position data */
-		glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
-		glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(float)* 4));
-
-		//draw to the screen
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_BYTE, NULL);
-
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-		glBindTexture(GL_TEXTURE_2D, 0);
 
 		//swap front and back buffers
 		glfwSwapBuffers(window);
@@ -391,9 +66,6 @@ int main()
 			glfwSetWindowShouldClose(window, GL_TRUE);
 	}
 
-
-	glDeleteBuffers(1, &uiIBO);
-
 	glfwTerminate();
 	return 0;
 }
@@ -402,8 +74,4 @@ void Update()
 {
 	Keys.Update();
 
-	if (Keys.IsDown(VK_UP))
-	{
-
-	}
 }
